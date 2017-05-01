9833a531db167e8ef05ef5d1aa0e6549490ac36a
perevalovds <perevalovds@gmail.com>
2017-05-01 17:46:09 +0500

    initial commit

---
+++ src/ofxKuTextGuiPreset.cpp
@@ -0,0 +1,17 @@
+﻿#include "ofxKuTextGuiPreset.h"
+
+
+//------------------------------------------------------------------------
+//function loads to preset system variables listed in file_var_list
+void ofxKuPreset_load_vars_to_preset_system(string file_var_list, ofxKuTextGui &gui, ofxKuPreset &presets) {
+
+}
+
+//------------------------------------------------------------------------
+
+
+
+
+
+
+
---
+++ src/ofxKuTextGuiPreset.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+/*
+ Optional module for integrating with presets.
+ Requires ofxKuPreset and ofxKuFile modules (part of ofxKu addon).
+*/
+
+#include "ofMain.h"
+#include "ofxKuTextGui.h"
+#include "ofxKuPreset.h"
+
+
+//function loads to preset system variables listed in file_var_list
+void ofxKuPreset_load_vars_to_preset_system(string file_var_list, ofxKuTextGui &gui, ofxKuPreset &presets);
+
+
+
+
+
+
+
+
+
