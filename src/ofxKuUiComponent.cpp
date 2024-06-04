49c16516ee7f320fd9239a7431de009447e7d8a2
Denis Perevalov <perevalovds@gmail.com>
2024-06-04 08:44:09 +0300

    polished

--- src/ofxKuUiComponent.cpp
+++ src/ofxKuUiComponent.cpp
@@ -3,5 +3,30 @@
 #include "json.hpp"
 
 //------------------------------------------------------------------------
+void KuUiComponent::setupSmoothValue() {
+	smoothed_value_normalized_ = valueNormalized();
+	smoothed_value_ = floatValue();
+}
 
+void KuUiComponent::updateSmoothedValue(float dt, float time_whole_change) {
+	float v = valueNormalized();
+	if (time_whole_change <= 0.00001) {
+		//momentary change
+		smoothed_value_normalized_ = v;
+	}
+	else {
+		//smooth change
+		float limit = dt / time_whole_change;
+		smoothed_value_normalized_ += ofClamp(v - smoothed_value_normalized_, -limit, limit);
+	}
+	smoothed_value_ = normalizedToValue(smoothed_value_normalized_);
+}
+
+float KuUiComponent::getSmoothedValue() {
+	return smoothed_value_;
+}
+
+void KuUiComponent::setDrawSmoothed(bool v) {
+	draw_smoothed_value_ = v;
+}
 //------------------------------------------------------------------------
--- src/ofxKuUiComponent.h
+++ src/ofxKuUiComponent.h
@@ -71,33 +71,13 @@ public:
 	float smoothed_value_ = 0;
 	bool draw_smoothed_value_ = false;
 
-	void setupSmoothValue() {
-		smoothed_value_normalized_ = valueNormalized();
-		smoothed_value_ = floatValue();
-	}
+	void setupSmoothValue();
 	//smoothing function
-	void updateSmoothedValue(float dt, float time_whole_change) {
-		float v = valueNormalized();
-		if (time_whole_change <= 0.00001) {
-			//momentary change
-			smoothed_value_normalized_ = v;
-		}
-		else {
-			//smooth change
-			float limit = dt / time_whole_change;
-			smoothed_value_normalized_ += ofClamp(v - smoothed_value_normalized_, -limit, limit);
-		}
-		smoothed_value_ = normalizedToValue(smoothed_value_normalized_);
-	}
-	//get smoother value
-	float getSmoothedValue() {
-		return smoothed_value_;
-	}
-
+	void updateSmoothedValue(float dt, float time_whole_change);
+	//get smoothed value
+	float getSmoothedValue();
 	//should we draw smoothed value
-	void setDrawSmoothed(bool v) {
-		draw_smoothed_value_ = v;
-	}
+	void setDrawSmoothed(bool v);
 };
 
 //------------------------------------------------------------------------
